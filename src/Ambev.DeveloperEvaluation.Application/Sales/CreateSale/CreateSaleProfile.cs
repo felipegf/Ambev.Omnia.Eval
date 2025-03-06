e726a824fb1366ff24f01c2b430d486d427ee78d
Felipe Gonçalves Ferreira <contato@felipegf.com.br>
2025-03-05 21:11:27 -0300

    feat(application): add command and handler for sale creation
    - Implemented `CreateSaleProfile` for DTO-to-entity mapping
    - Created `CreateSaleResult` to encapsulate the operation result

---
+++ src/Ambev.DeveloperEvaluation.Application/Sales/CreateSale/CreateSaleProfile.cs
@@ -0,0 +1,28 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Entities;
+using AutoMapper;
+
+namespace Ambev.DeveloperEvaluation.Application.Sales.CreateSale;
+
+/// <summary>
+/// Profile responsible for mapping CreateSaleCommand to Sale entity.
+/// </summary>
+public class CreateSaleProfile : Profile
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="CreateSaleProfile"/> class.
+    /// Configures mappings between CreateSaleCommand and Sale entities.
+    /// </summary>
+    public CreateSaleProfile()
+    {
+        CreateMap<CreateSaleCommand, Sale>()
+            .ForMember(dest => dest.Id, opt => opt.Ignore()) // ID is generated
+            .ForMember(dest => dest.CreatedAt, opt => opt.Ignore()) // Handled internally
+            .ForMember(dest => dest.UpdatedAt, opt => opt.Ignore()) // Handled internally
+            .ForMember(dest => dest.SaleItems, opt => opt.MapFrom(src => src.Items));
+
+        CreateMap<CreateSaleItemCommand, SaleItem>()
+            .ForMember(dest => dest.Id, opt => opt.Ignore()) // ID is generated
+            .ForMember(dest => dest.SaleId, opt => opt.Ignore()) // Set in repository
+            .ForMember(dest => dest.TotalAmount, opt => opt.Ignore()); // Calculated
+    }
+}
---
+++ src/Ambev.DeveloperEvaluation.Application/Sales/CreateSale/CreateSaleResult.cs
@@ -0,0 +1,17 @@
+﻿namespace Ambev.DeveloperEvaluation.Application.Sales.CreateSale;
+
+/// <summary>
+/// Represents the result of a successful sale creation.
+/// </summary>
+public sealed class CreateSaleResult
+{
+    /// <summary>
+    /// Gets or sets the unique identifier of the sale.
+    /// </summary>
+    public Guid SaleId { get; set; }
+
+    /// <summary>
+    /// Gets or sets the total amount of the sale.
+    /// </summary>
+    public decimal TotalAmount { get; set; }
+}
