2dfe98a61e54d9d6b335ceb85f804832564fb31f
Felipe Gonçalves Ferreira <contato@felipegf.com.br>
2025-03-06 18:48:15 -0300

    feat(application): implement GetAllSales query and handler
    - Added `GetAllSalesQuery` for retrieving all sales
    - Implemented `GetAllSalesHandler` to handle the query
    - Integrated AutoMapper to map sales to `GetSaleResult`

---
+++ src/Ambev.DeveloperEvaluation.Application/Sales/GetAllSales/GetAllSalesQuery.cs
@@ -0,0 +1,9 @@
+﻿using Ambev.DeveloperEvaluation.Application.Sales.GetSale;
+using MediatR;
+
+namespace Ambev.DeveloperEvaluation.Application.Sales.GetAllSales;
+
+/// <summary>
+/// Query to retrieve all sales.
+/// </summary>
+public record GetAllSalesQuery() : IRequest<List<GetSaleResult>>;
