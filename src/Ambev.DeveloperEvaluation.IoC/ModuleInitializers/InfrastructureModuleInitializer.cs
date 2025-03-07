f357ab3d9130432afd60252bfcc164535c9c8471
Felipe Gonçalves Ferreira <contato@felipegf.com.br>
2025-03-07 19:14:44 -0300

    feat(ioc): register DiscountService in dependency injection
    - Added `IDiscountService` and `DiscountService` to DI container in `InfrastructureModuleInitializer`.
    - Enables discount calculation logic throughout the application.

--- src/Ambev.DeveloperEvaluation.IoC/ModuleInitializers/InfrastructureModuleInitializer.cs
+++ src/Ambev.DeveloperEvaluation.IoC/ModuleInitializers/InfrastructureModuleInitializer.cs
@@ -1,4 +1,5 @@
 ﻿using Ambev.DeveloperEvaluation.Domain.Repositories;
+using Ambev.DeveloperEvaluation.Domain.Services;
 using Ambev.DeveloperEvaluation.ORM;
 using Ambev.DeveloperEvaluation.ORM.Repositories;
 using Ambev.DeveloperEvaluation.ORM.UoW;
@@ -13,6 +14,7 @@ public class InfrastructureModuleInitializer : IModuleInitializer
     public void Initialize(WebApplicationBuilder builder)
     {
         builder.Services.AddScoped<DbContext>(provider => provider.GetRequiredService<DefaultContext>());
+        builder.Services.AddScoped<IDiscountService, DiscountService>();
         builder.Services.AddScoped<IUserRepository, UserRepository>();
         builder.Services.AddScoped<ISaleRepository, SaleRepository>();
         builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
