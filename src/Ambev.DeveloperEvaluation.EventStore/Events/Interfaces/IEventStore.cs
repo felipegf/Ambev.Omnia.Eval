57e7a18539b608a9e872702d11b79465556c7784
Felipe Gonçalves Ferreira <contato@felipegf.com.br>
2025-03-11 18:33:12 -0300

    ✨ feat(event-store): introduce MongoDB event store
    - Added IEventStore interface for persisting events.
    - Implemented MongoEventStorage to store events in MongoDB.
    - Integrated MongoEventStorage with NoSqlContext.

---
+++ src/Ambev.DeveloperEvaluation.EventStore/Events/Interfaces/IEventStore.cs
@@ -0,0 +1,22 @@
+﻿using Ambev.DeveloperEvaluation.Common.Events;
+
+namespace Ambev.DeveloperEvaluation.EventLog.Events.Interfaces;
+
+/// <summary>
+/// Defines an event store responsible for persisting domain events.
+/// </summary>
+public interface IEventStore
+{
+    /// <summary>
+    /// Saves an event in the event store.
+    /// </summary>
+    /// <param name="event">The event to be stored.</param>
+    Task SaveEventAsync(IEvent @event);
+
+    /// <summary>
+    /// Retrieves all events related to a specific aggregate.
+    /// </summary>
+    /// <param name="aggregateId">The aggregate identifier.</param>
+    /// <returns>A list of events.</returns>
+    Task<IEnumerable<IEvent>> GetEventsByAggregateIdAsync(Guid aggregateId);
+}
---
+++ src/Ambev.DeveloperEvaluation.EventStore/Events/Persistence/MongoEventStorage.cs
@@ -0,0 +1,36 @@
+﻿using Ambev.DeveloperEvaluation.Common.Events;
+using Ambev.DeveloperEvaluation.EventLog.Events.Interfaces;
+using MongoDB.Driver;
+
+namespace Ambev.DeveloperEvaluation.EventLog.Events.Persistence;
+
+/// <summary>
+/// MongoDB implementation of <see cref="IEventStore"/> for storing domain events.
+/// </summary>
+public class MongoEventStorage : IEventStore
+{
+    private readonly NoSqlContext _context;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="MongoEventStorage"/> class.
+    /// </summary>
+    /// <param name="noSqlContext">The NoSQL database context.</param>
+    public MongoEventStorage(NoSqlContext noSqlContext)
+    {
+        _context = noSqlContext;
+    }
+
+    /// <inheritdoc />
+    public async Task SaveEventAsync(IEvent @event)
+    {
+        await _context.EventLogs.InsertOneAsync(@event);
+    }
+
+    /// <inheritdoc />
+    public async Task<IEnumerable<IEvent>> GetEventsByAggregateIdAsync(Guid aggregateId)
+    {
+        return await _context.EventLogs
+            .Find(e => e.AggregateId == aggregateId)
+            .ToListAsync();
+    }
+}
