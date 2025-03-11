64606d626c5f4c51bcd030443f234baab70ac411
Felipe Gonçalves Ferreira <contato@felipegf.com.br>
2025-03-11 18:33:58 -0300

    ✨ feat(log-storage): add MongoDB-based log storage
    - Introduced ILogStorage interface for logging.
    - Implemented MongoLogStorage to persist logs in MongoDB.
    - Added LogEntry entity.

---
+++ src/Ambev.DeveloperEvaluation.EventStore/LogStorage/Persistence/MongoLogStorage.cs
@@ -0,0 +1,27 @@
+﻿using Ambev.DeveloperEvaluation.EventLog.LogStorage.Entities;
+using Ambev.DeveloperEvaluation.EventLog.LogStorage.Interfaces;
+
+namespace Ambev.DeveloperEvaluation.EventLog.LogStorage.Persistence;
+
+/// <summary>
+/// MongoDB implementation of <see cref="ILogStorage"/>.
+/// </summary>
+public class MongoLogStorage : ILogStorage
+{
+    private readonly NoSqlContext _context;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="MongoLogStorage"/> class.
+    /// </summary>
+    /// <param name="noSqlContext">The NoSQL database context.</param>
+    public MongoLogStorage(NoSqlContext noSqlContext)
+    {
+        _context = noSqlContext;
+    }
+
+    /// <inheritdoc />
+    public async Task StoreLogAsync(LogEntry logEntry)
+    {
+        await _context.ApplicationLogs.InsertOneAsync(logEntry);
+    }
+}
